0ef96d6d53ebe74b3bc3b2994d6f5166d0a79418
BhushanShirude <bhushanshirude95@gmail.com>
2018-05-03 19:01:04 +0530

    Email Send

--- Server/controller/invitation.js
+++ Server/controller/invitation.js
@@ -4,39 +4,63 @@ var multer = require("multer")
 var config = require("../config/config");
 
 module.exports = {
-    create: function(request, response) {
-        var newUser = new invitationModel(request.body);
-        newUser.save({ $set: request.body },
-            function(err, docs) {
-                console.log("============body===========", request.body)
-                if (err) {
-                    response.status(500).json({ status: "Error", message: err, docs: '' });
-                    return false;
-                }
-                response.status(200).json({ status: "Success", message: "Success", docs: docs });
-            })
-    },
-    // send: function(request, response) {
-    //     // console.log("==============Status===============", request.params.id)
-    //     invitationModel.findOne({ _id: request.params.id },
-    //         function(err, doc) {
-    //             if (err || !doc) {
-    //                 response.status(500).json({ status: "Error", message: err | "User does not exist", docs: '' });
+    // create: function(request, response) {
+    //     var newUser = new invitationModel(request.body);
+    //     newUser.save({ $set: request.body },
+    //         function(err, docs) {
+    //             console.log("============body===========", request.body)
+    //             if (err) {
+    //                 response.status(500).json({ status: "Error", message: err, docs: '' });
     //                 return false;
     //             }
-    //             // if user found
-    //             var email = doc.personalDetails.Email;
-    //             var url = config.WEBURL;
-    //             var msg = "Hello " + doc.personalDetails.FirstName + ", you are select in second round you can Accept or Reject Invitation of second Round.<br>";
-    //             msg += "<a href='" + url + "'>Accpet</a> <br>";
-    //             msg += "<a href='" + url + "'>Reject</a> <br>";
-    //             if (sendEmail(email, "status", msg)) {
-    //                 response.status(500).json({ status: "Error", message: "Mail count not be sent! Please try after some time", docs: "" });
-    //                 return false;
-    //             }
-    //             response.status(200).json({ status: "Ok", message: "Mail has been sent!", docs: "" });
+    //             response.status(200).json({ status: "Success", message: "Success", docs: docs });
     //         })
     // },
+
+
+    create: function(req, response) {
+
+        var newUser = new invitationModel(req.body);
+
+        newUser.save(function(err) {
+            if (err) {
+                response.status(500).json({ status: "Error", message: err, docs: '' });
+                return false;
+            }
+            var url = config.WEBURL + "home/verify/" + newUser._id;
+
+            var msg = "Hello " + newUser.InvitationDetails.Sname + " " + newUser.InvitationDetails.Sname + ", Here is your verification link.";
+
+            msg += "<a href='" + url + "'>Verify</a > Create Password Thank you.";
+
+            if (sendEmail(newUser.InvitationDetails.Semail, "Verification Email", msg)) {
+                response.json({ status: "Email Error", message: "Email could not sent!" });
+                return false;
+            }
+            response.status(200).json({ status: "Success", message: "Success", docs: '' });
+        });
+    },
+    send: function(request, response) {
+        // console.log("==============Status===============", request.params.id)
+        invitationModel.findOne({ _id: request.params.id },
+            function(err, doc) {
+                if (err || !doc) {
+                    response.status(500).json({ status: "Error", message: err | "User does not exist", docs: '' });
+                    return false;
+                }
+                // if user found
+                var email = doc.InvitationDetails.Semail;
+                var url = config.WEBURL;
+                var msg = "Hello " + doc.InvitationDetails.Sname + ", you are select in second round you can Accept or Reject Invitation of second Round.<br>";
+                msg += "<a href='" + url + "'>Accpet</a> <br>";
+                msg += "<a href='" + url + "'>Reject</a> <br>";
+                if (sendEmail(email, "status", msg)) {
+                    response.status(500).json({ status: "Error", message: "Mail count not be sent! Please try after some time", docs: "" });
+                    return false;
+                }
+                response.status(200).json({ status: "Ok", message: "Mail has been sent!", docs: "" });
+            })
+    },
 }
 
 function sendEmail(to, sub, msg) {
--- src/messagesend/messagesend.component.ts
+++ src/messagesend/messagesend.component.ts
@@ -11,9 +11,9 @@ import { httpService } from '../httpservice';
 export class MessagesendComponent implements OnInit {
   private userData;
   private meetingData;
-  constructor(private HttpService: httpService, private router: Router) { 
-    this.userData=JSON.parse(localStorage.getItem("user"));
-    console.log("================InviData1=======",this.userData['0']._id)
+  constructor(private HttpService: httpService, private router: Router) {
+    this.userData = JSON.parse(localStorage.getItem("user"));
+    console.log("================InviData1=======", this.userData)
   }
 
   ngOnInit() {
@@ -23,17 +23,20 @@ export class MessagesendComponent implements OnInit {
     this.router.navigate(['home/event/meetingsch']);
   }
   schedule(form: any, event: Event) {
-    let inviData ={
-      "InvitationDetails":form.value,
-      "UserId":this.userData['0']._id
+    let inviData = {
+      "InvitationDetails": form.value,
+      "UserId": this.userData['0']._id
     }
-    this.HttpService.post("invitation",inviData).subscribe(
-      resp=>{
-        console.log("=======Success=========",resp)     
+    this.HttpService.post("invitation", inviData).subscribe(
+      resp => {
+        console.log("=======Success=========", resp)
         swal("Thanks", "Schedule Mail Has Been Send", "success")
-    },err=>{
-      console.log("=========Error=========",err)   
-    });
+      }, err => {
+        console.log("=========Error=========", err)
+      });
+    form.resetForm();
+
+    return true;
   }
 
 }
