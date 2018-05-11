cd09fee4a718c706785d4392defd2bbacd252b96
BhushanShirude <bhushanshirude95@gmail.com>
2018-05-11 16:55:58 +0530

    Secand last Commit

--- src/home.ts
+++ src/home.ts
@@ -1,10 +1,14 @@
 import {Component, OnInit}from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-home',
     templateUrl: 'home.html'
 })
 export class HomeComponent implements OnInit{
-    constructor(){}
-    ngOnInit(){}
+    constructor(private router:Router){
+    // localStorage.clear();
+}
+    ngOnInit(){
+    }
 }
